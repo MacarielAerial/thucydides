eab56911ae9692064928ec95ad6377ffbc85fbd3
Yifei Yu <yifei@freyda.io>
2022-01-29 13:22:25 +0000

    Update environment

--- tests/data/data_registry.py
+++ tests/data/data_registry.py
@@ -2,6 +2,6 @@ from pathlib import Path
 
 PATH_NEWS_JSON_UNTYPED_1: Path = Path("tests") / "data" / "unit" / "a-52104029.json"
 PATH_NEWS_JSON_UNTYPED_2: Path = Path("tests") / "data" / "unit" / "a-67488720.json"
-PATH_NEWS_JSONL_UNTYPED_OUTPUT: Path = Path(
-    "tests"
-) / "data" / "output" / "news_jsonl_untyped.jsonl"
+PATH_NEWS_JSONL_UNTYPED_OUTPUT: Path = (
+    Path("tests") / "data" / "output" / "news_jsonl_untyped.jsonl"
+)
